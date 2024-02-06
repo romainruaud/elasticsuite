ca1beeaf59d86a6f148eb57632041b7545eb47af
Romain Ruaud <romain.ruaud@smile.fr>
2024-02-06 17:31:36 +0100

    Adding auto select for Country in HS form into adminhtml.

--- src/module-elasticsuite-tracker/Block/Adminhtml/Hs.php
+++ src/module-elasticsuite-tracker/Block/Adminhtml/Hs.php
@@ -30,19 +30,28 @@ class Hs extends \Magento\Backend\Block\Template
     private \Magento\Backend\Model\Auth\Session $authSession;
 
     /**
+     * @var \Magento\Directory\Model\CountryFactory
+     */
+    private \Magento\Directory\Model\CountryFactory $countryFactory;
+
+    /**
      * Block for HS form.
      *
-     * @param \Magento\Backend\Block\Template\Context $context     Adminhtml context
-     * @param \Magento\Backend\Model\Auth\Session     $authSession Adminhtml session
-     * @param array                                   $data        Block data
+     * @param \Magento\Backend\Block\Template\Context $context        Adminhtml context
+     * @param \Magento\Backend\Model\Auth\Session     $authSession    Adminhtml session
+     * @param \Magento\Directory\Model\CountryFactory $countryFactory Country Factory
+     * @param array                                   $data           Block data
      */
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
         \Magento\Backend\Model\Auth\Session $authSession,
+        \Magento\Directory\Model\CountryFactory $countryFactory,
         array $data = []
     ) {
-        $this->authSession = $authSession;
+        $this->authSession    = $authSession;
+        $this->countryFactory = $countryFactory;
         parent::__construct($context, $data);
+        $this->directoryHelper = $this->getData('directoryHelper');
     }
 
     /**
@@ -53,5 +62,30 @@ class Hs extends \Magento\Backend\Block\Template
     public function getEmail()
     {
         return (string) $this->authSession->getUser()->getEmail();
+    }
+
+    /**
+     * Get current user country.
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        $hsCountries = [
+            "France",
+            "Belgium",
+            "Netherlands",
+            "Luxembourg",
+            "Switzerland",
+            "Germany",
+            "Morocoo",
+            "Ukraine"
+        ];
+
+        $userCountryCode = $this->directoryHelper->getDefaultCountry($this->_storeManager->getDefaultStoreView()->getId());
+        $country         = $this->countryFactory->create()->loadByCode($userCountryCode);
+        $countryName     = $country->getName("en_US");
+
+        return (in_array($countryName, $hsCountries)) ? $countryName : "Others";
     }
 }
